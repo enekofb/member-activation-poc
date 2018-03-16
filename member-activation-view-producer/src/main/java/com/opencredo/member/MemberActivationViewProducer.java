75014e0ba994332c2ac9101e81ec6ae8d4dffbee
eneko <eneko.fernandez@opencredo.com>
2018-03-16 17:09:30 +0000

    [member view producer poc] can query by activation key

--- member-activation-view-producer/src/main/java/com/opencredo/member/MemberActivationViewProducer.java
+++ member-activation-view-producer/src/main/java/com/opencredo/member/MemberActivationViewProducer.java
@@ -3,15 +3,15 @@ package com.opencredo.member;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 
 @Value
 @Slf4j
@@ -23,7 +23,7 @@ public class MemberActivationViewProducer {
     private final Table memberActivationsTable;
     private final AmazonDynamoDB client;
 
-    public MemberActivationViewProducer(){
+    public MemberActivationViewProducer() {
         this.client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.EU_WEST_1)
                 .build();
@@ -54,9 +54,27 @@ public class MemberActivationViewProducer {
 
         Item memberActivationItem = new Item()
                 .withPrimaryKey("activationTime", activationTimePk)
-                .withString("activationTimestamp",memberActivationEvent.getActivationTimestamp());
+                .withString("activationTimestamp", memberActivationEvent.getActivationTimestamp());
 
         return memberActivationItem;
+
+    }
+
+    public ItemCollection<QueryOutcome> getAllMemberActivationEventByPartitionKey(String activationTime) {
+
+
+        QuerySpec spec = new QuerySpec().withKeyConditionExpression("activationTime = :v_id")
+                .withValueMap(new ValueMap().withString(":v_id", activationTime));
+
+        ItemCollection<QueryOutcome> items = memberActivationsTable.query(spec);
+
+
+        Iterator<Item> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            log.info("Found {}", iterator.next().toJSONPretty());
+
+        }
+        return items;
 
     }
 }
--- member-activation-view-producer/src/test/java/com/opencredo/member/MemberActivationDynamodbClientTest.java
+++ member-activation-view-producer/src/test/java/com/opencredo/member/MemberActivationDynamodbClientTest.java
@@ -1,10 +1,14 @@
 package com.opencredo.member;
 
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.Assert.assertTrue;
@@ -22,7 +26,7 @@ public class MemberActivationDynamodbClientTest {
     }
 
     @Test
-    public void canWritetToMemberActivationTable(){
+    public void canWritetToMemberActivationEvent(){
         MemberActivationEvent memberActivationEvent = createMemberActivationEvent();
         boolean memberActivationWritten = memberActivationViewProducer.writeMemberActivationEvent(memberActivationEvent);
         assertTrue(memberActivationWritten);
@@ -33,6 +37,14 @@ public class MemberActivationDynamodbClientTest {
         String activationDateTimeAsString = activationDateTime.toString();
         log.info("Activation date {}",activationDateTimeAsString);
         return new MemberActivationEvent(activationDateTimeAsString);
+    }
+
+
+    @Test
+    public void canGetAllMemberActivationEventByActivationkey(){
+        String activationTimePk = LocalDate.now().toString();
+        ItemCollection<QueryOutcome> memberActivationWritten = memberActivationViewProducer.getAllMemberActivationEventByPartitionKey(activationTimePk);
+        assertTrue(memberActivationWritten.getAccumulatedItemCount() > 0 );
     }
 
 }
